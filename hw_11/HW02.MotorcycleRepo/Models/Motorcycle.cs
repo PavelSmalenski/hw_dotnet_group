62f88fa6576605fd6c6996f49f943c2615348b67
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-07-22 22:54:12 +0300

    Add stub fields to Motorcycle

--- hw_11/HW02.MotorcycleRepo/Models/Motorcycle.cs
+++ hw_11/HW02.MotorcycleRepo/Models/Motorcycle.cs
@@ -9,7 +9,12 @@ namespace HW02.MotorcycleRepo.Models
     /// </summary>
     class Motorcycle
     {
-        public Motorcycle(string name, string model, string yead, int odometer)
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Year { get; set; }
+        public string Odometer { get; set; }
+
+        public Motorcycle(string name, string model, string year, int odometer)
         {
             throw new NotImplementedException();
         }
