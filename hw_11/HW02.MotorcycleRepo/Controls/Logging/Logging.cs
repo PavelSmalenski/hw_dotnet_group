c676b18deebf6abcde3117e01ce10d2a8123afcd
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-07-22 14:54:04 +0300

    Add stub for logging class

---
+++ hw_11/HW02.MotorcycleRepo/Controls/Logging/Logging.cs
@@ -0,0 +1,30 @@
+﻿using Serilog;
+using Serilog.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW02.MotorcycleRepo.Controls.Logging
+{
+    public enum LogType
+    {
+        Application,
+        CRUD,
+        ConsoleInOut,
+        ProgramFinish,
+        Exception
+    }
+
+    /// <summary>
+    /// Implementation of singleton for loggers of LogType type
+    /// </summary>
+    static class Logging
+    {
+        static Dictionary<LogType, Logger> _loggers = new Dictionary<LogType, Logger>();
+
+        public static Logger GetLogger(LogType logType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
