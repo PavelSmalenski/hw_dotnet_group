54b5206dadb3cc6fcba92698d10c4cbc2cb77fe0
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-07-22 15:18:09 +0300

    Add motorcycle repository entities stubs

---
+++ hw_11/HW02.MotorcycleRepo/Controls/Repositories/IMotorcycleRepository.cs
@@ -0,0 +1,19 @@
+﻿using HW02.MotorcycleRepo.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW02.MotorcycleRepo.Controls.Repositories
+{
+    /// <summary>
+    /// Interface for CRUD Repository of Motorcycles
+    /// </summary>
+    interface IMotorcycleRepository
+    {
+        public void Add(Motorcycle motorcycle);
+        public IEnumerable<Motorcycle> GetAll();
+        public Motorcycle GetById(string id);
+        public void Delete(string id);
+        public void Update(Motorcycle motorcycle);
+    }
+}
---
+++ hw_11/HW02.MotorcycleRepo/Models/Motorcycle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW02.MotorcycleRepo.Models
+{
+    /// <summary>
+    /// Motorcycle item representation
+    /// </summary>
+    class Motorcycle
+    {
+        public Motorcycle(string name, string model, string yead, int odometer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
