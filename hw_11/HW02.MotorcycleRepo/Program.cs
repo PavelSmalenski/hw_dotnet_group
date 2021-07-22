9c15a496b77671281f8dd7f981dc7e2bf50fa8c1
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-07-22 14:11:14 +0300

    Add stub projects for hw_11

---
+++ hw_11/HW02.MotorcycleRepo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW02.MotorcycleRepo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
