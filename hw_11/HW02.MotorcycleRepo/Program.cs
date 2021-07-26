97190d71907addf34a2227492d56213a22c2f189
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-07-26 21:31:04 +0300

    Add program to call repositories

--- hw_11/HW02.MotorcycleRepo/Program.cs
+++ hw_11/HW02.MotorcycleRepo/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using HW02.MotorcycleRepo.Controls.Logging;
+using HW02.MotorcycleRepo.Controls.Repositories;
+using HW02.MotorcycleRepo.Models;
+using System;
 
 namespace HW02.MotorcycleRepo
 {
@@ -6,7 +9,76 @@ namespace HW02.MotorcycleRepo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StartProgram();
+
+            // Init static repo using data from DB repo
+            var dbRepo = MotorcycleRepositoryProvider.DBRepository;
+            var staticRepo = MotorcycleRepositoryProvider.StaticRepository;
+            var dbMotorcycles = dbRepo.GetAll();
+            foreach (var motorcycle in dbMotorcycles)
+            {
+                staticRepo.Add(motorcycle);
+            }
+
+            TestRepository(dbRepo);
+            TestRepository(staticRepo);
+
+            FinishProgram();
+            Console.ReadLine();
+        }
+
+        public static void TestRepository(IMotorcycleRepository repo)
+        {
+            // Read
+            Motorcycle existingMotorcycle;
+            try
+            {
+                existingMotorcycle = repo.GetAll()[0];
+                Console.WriteLine($"READ - item: {existingMotorcycle}");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Logging.GetLogger(LogType.Exception).Error($"Repository should have at least 1 item for testing!");
+                return;
+            }
+
+            // Update
+            string oldName = existingMotorcycle.Name;
+            existingMotorcycle.Name = "New Name";
+
+            repo.Update(existingMotorcycle);
+            var repoMotorcycle = repo.GetById(existingMotorcycle.Id);
+            Console.WriteLine($"UPDATE - item BEFORE: {repoMotorcycle}");
+
+            existingMotorcycle.Name = oldName;
+            repo.Update(existingMotorcycle);
+            repoMotorcycle = repo.GetById(existingMotorcycle.Id);
+            Console.WriteLine($"UPDATE - item AFTER: {repoMotorcycle}");
+
+            // Delete - Add
+            repo.Delete(existingMotorcycle.Id);
+            Console.WriteLine($"DELETE - repo after delete:");
+            foreach (var motorcycle in repo.GetAll())
+            {
+                Console.WriteLine(motorcycle);
+            }
+
+            repo.Add(existingMotorcycle);
+            Console.WriteLine($"ADD - repo after add:");
+            foreach (var motorcycle in repo.GetAll())
+            {
+                Console.WriteLine(motorcycle);
+            }
+        }
+
+        public static void StartProgram()
+        {
+            Logging.GetLogger(LogType.Application).Information("Application started");
+        }
+
+        public static void FinishProgram()
+        {
+            Logging.GetLogger(LogType.ProgramFinish).Information("Application finished");
         }
     }
 }
