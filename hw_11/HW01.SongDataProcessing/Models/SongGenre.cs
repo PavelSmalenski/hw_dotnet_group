5aae0d34947687938099b696069168e21a88e710
Pavel Smalenski <smolenski.p.a@gmail.com>
2021-07-27 21:19:15 +0300

    Add implementation of task 1 - serialization

---
+++ hw_11/HW01.SongDataProcessing/Models/SongGenre.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW01.SongDataProcessing.Models
+{
+    enum SongGenre
+    {
+        Classic,
+        Rock,
+        Jazz
+    }
+}
